bf0c10f34664c0fa8e57041cc2da071311f98213
Jason Kulak <JasonKulak@yahoo.com>
2024-02-04 17:48:09 -0600

    getting started and setting up the components

---
+++ components/Button.js
@@ -0,0 +1,11 @@
+// import {useState} from "react"
+
+function Button(props) {
+    return (
+        <button>
+            Get a Gif
+        </button>
+    )
+  }
+  
+  export default Button;
--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+// import Button from "./components/Button";
+// import Gifs from "./components/Gifs";
+// import Header from "./components/Header";
+import {useState, useEffect} from "react";
+
 
 function App() {
+
+  //apikey
+  const apiKey = "T49vkgIn68gSf1nOa9R8RWmbm3IWruUj"
+
+  //state
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello giphy assignment</h1>
+      <button>Get a Gif</button>
     </div>
   );
 }
